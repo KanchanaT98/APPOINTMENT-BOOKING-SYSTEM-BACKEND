d9bc32a76b3e19fc65013745b913d42106f5647f
KanchanaT98 <thennakoon.kanchana@gmail.com>
2025-02-14 01:40:29 +0530

    Initialized the project

--- src/main/java/com/example/demo/AppointmentSystemApplication.java
+++ src/main/java/com/example/demo/AppointmentSystemApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppointmentSystemApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AppointmentSystemApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner runner() {
+		return args -> {
+			System.out.println("Application is running...");
+		};
+	}
+
 
 }
